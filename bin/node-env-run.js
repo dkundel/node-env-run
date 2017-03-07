5d989706b413eae24f715839853d0309bd467d35
Dominik Kundel <dominik.kundel@gmail.com>
2017-03-07 14:43:37 +0100

    :hammer: Improve logging

--- bin/node-env-run.js
+++ bin/node-env-run.js
@@ -7,6 +7,10 @@ const { spawn } = require('child_process');
 const args = require('args');
 const dotenv = require('dotenv');
 
+const { logger } = require('../lib/utils');
+
+const cwd = process.cwd();
+
 args
   .option(['f', 'force'], 'Temporarily overrides existing env variables with the ones in the .env file')
   .option(['E', 'env'], 'Location of .env file relative from the current working directory', '.env')
@@ -16,27 +20,28 @@ args
 args.config.value = '<script to execute>';
 
 const flags = args.parse(process.argv);
-const cwd = process.cwd();
+const log = logger(flags.verbose)
 
 let scriptToExecute = args.sub[0];
 
 if (!scriptToExecute) {
-  console.error('You need to specify a script to run or alternatively "." to run the script specified in "main" in your "package.json"');
+  log.error('You need to specify a script to run or alternatively "." to run the script specified in "main" in your "package.json"');
   process.exit(1);
 }
 
 
 if (scriptToExecute === '.') {
+  log.debug('Evalute package.json to determine script to execute.');
   const pathToPkg = path.resolve(cwd, 'package.json');
   if (!fs.existsSync(pathToPkg)) {
-    console.error('In order to use "." you need to have a package.json in the current working directory.');
+    log.error('In order to use "." you need to have a package.json in the current working directory.');
     process.exit(1);
   } 
 
   const pkg = require(pathToPkg);
   
   if (!pkg.main) {
-    console.error('Could not find a "main" entry in the package.json');
+    log.error('Could not find a "main" entry in the package.json');
     process.exit(1);
   }
 
@@ -45,7 +50,14 @@ if (scriptToExecute === '.') {
   scriptToExecute = path.resolve(cwd, scriptToExecute);
 }
 
-const envContent = fs.readFileSync(path.resolve(cwd, flags.env))
+const envFilePath = path.resolve(cwd, flags.env);
+if (!fs.existsSync) {
+  log.error(`Could not find the .env file under: "${envFilePath}"`);
+  process.exit(1);
+}
+
+log.debug('Reading .env file');
+const envContent = fs.readFileSync(path.resolve(cwd, flags.env));
 const envValues = dotenv.parse(envContent);
 
 const envKeys = Object.keys(envValues).filter(key => {
@@ -60,9 +72,7 @@ envKeys.forEach(key => {
   process.env[key] = envValues[key];
 });
 
-if (flags.verbose) {
-  console.log(`Set the env variables: ${envKeys.map(k => `"${k}"`).join(',')}`);
-  console.log(`Execute script: ${scriptToExecute}`);
-}
+log.debug(`Set the env variables: ${envKeys.map(k => `"${k}"`).join(',')}`);
+log.debug(`Execute script: ${scriptToExecute}`);
 
 require(scriptToExecute);
